7f8e534914d344049178c8ca1aef9ad50ac6f46f
ed <cabrale234@gmail.com>
2022-02-08 14:14:49 -0500

    navbar

---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,42 @@
+import React, {useState, Component} from "react";
+import { MenuItems } from "./MenuItems"
+import { FaBars, FaTimes } from 'react-icons/fa'
+import { Button } from "./Button";
+
+
+function Navbar(props) {
+
+    const [clicked, setClicked] = useState(false)
+
+    const handleClick = () => {
+        setClicked(!clicked)
+    }
+
+    return (
+    
+    <nav className="NavbarItems">
+        <h1 className="navbar-logo">Skate Shop<i className="fab fa-react"></i></h1>
+        <div className="menu-icon" onClick={handleClick}>
+            <i className={clicked ? 'fas  fa-times' : 'fas fa-bars'}>
+                    {clicked ? <FaTimes /> : <FaBars/>}
+            </i>
+        </div>
+        <ul className={clicked ? 'nav-menu active' : 'nav-menu'}>
+            {MenuItems.map((item, index) => {
+                return (
+                    <li>
+                        <a className={item.cName} href={item.url}>
+                            {item.title}
+                        </a>
+                    </li>
+                )
+            })}
+        </ul>
+        <Button>Signup</Button>
+    </nav>
+    
+        
+    )
+}
+
+export default Navbar
